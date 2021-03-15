50504560dbbaad10a907c555102d353936b901d3
Md. Jahid Hasan <jhpabel@gmail.com>
2021-03-16 04:22:25 +0600

    push build public

---
+++ public/precache-manifest.3753d5b177864edd65b959aea2780edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406a7d7e5f7fa0f04819aae61d0636bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d089fd2876640565a4",
+    "url": "/static/css/main.eb794fda.chunk.css"
+  },
+  {
+    "revision": "7e8fdee4e34229be6875",
+    "url": "/static/js/2.656e2a32.chunk.js"
+  },
+  {
+    "revision": "8f5af6f500f0ad84ae113444f27b607c",
+    "url": "/static/js/2.656e2a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d089fd2876640565a4",
+    "url": "/static/js/main.98e6564c.chunk.js"
+  },
+  {
+    "revision": "142d6829b916f62f2bb9",
+    "url": "/static/js/runtime-main.046262a6.js"
+  },
+  {
+    "revision": "cd997544dcb6d1c835b181a874f9b806",
+    "url": "/static/media/spacex.cd997544.png"
+  }
+]);
